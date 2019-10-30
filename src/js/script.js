31467170ff7a612e428c6a631b6dc9d8173b3dcf
dmitriydv1990 <dmitriydv1990@gmail.com>
2019-10-30 14:27:48 +0600

    section catalog is done

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,43 @@
 $(document).ready(function(){
+
+    // скрипт слайдера
+    
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/prevArrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/nextArrow.svg"></button>'   
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/nextArrow.svg"></button>',  
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
+
+    // скрипт для табов
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    // скрипт для карточек (добавление и удаление класса active)
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
